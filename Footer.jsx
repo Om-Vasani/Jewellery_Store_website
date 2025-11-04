de9acae9e3e4d328865ff707cb7c9f4d280cfb3a
Om-Vasani <omvasani63@gmail.com>
2025-11-04 12:41:34 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,2 +1,11 @@
-import React from 'react'
-export default function Footer(){ return (<footer className="footer"><div className="container">© {new Date().getFullYear()} Amidip Jewellers — All rights reserved.</div></footer>) }
+import React from "react";
+
+export default function Footer(){
+  return (
+    <footer className="site-footer">
+      <div className="container small">
+        © {new Date().getFullYear()} Amidip Jewellers — All rights reserved.
+      </div>
+    </footer>
+  );
+}
