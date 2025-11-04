a7c676f0dc15b949f77e35708b15b9a20d253ae7
Om-Vasani <omvasani63@gmail.com>
2025-11-04 10:45:40 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,28 +1,10 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Navbar from './Navbar'
-import Home from './Home'
-import Catalog from './Catalog'
-import About from './About'
-import Contact from './Contact'
-import Admin from './Admin'
-import Footer from './Footer'
 
-export default function App(){
+export default function App() {
   return (
-    <div className="app-root">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/catalog" element={<Catalog/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/admin" element={<Admin/>} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </main>
-      <Footer />
+    <div style={{fontFamily: 'sans-serif', padding: 20}}>
+      <h1>Jewellery Store Demo App</h1>
+      <p>If you see this page, build entry is correct.</p>
     </div>
   )
 }
